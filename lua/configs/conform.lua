b1bc2331d9f0cc0d9b10cc198c0d61317e6f3be5
bhaveshsinghal95182 <work.bhavesh22@gmail.com>
2025-11-09 16:51:15 +0530

    removed timeout after save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,15 +3,14 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    typesript = {"prettier"},
-    javascript = {"prettier"},
-    typescriptreact = {"prettier"},
-    javascriptreact = {"prettier"},
+    typesript = { "prettier" },
+    javascript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
   },
 
   format_on_save = {
-  --   -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    --   -- These options will be passed to conform.format()
     lsp_fallback = true,
   },
 }
